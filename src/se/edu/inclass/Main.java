4017977c2beb094feb182d32ef4769afdc40a2b1
mhchan163 <mhchan163@gmail.com>
2020-09-18 17:05:19 +0800

    Added stuff

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,6 +21,10 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDataUsingStream(tasksData);
+        printDeadlineUsingStream(tasksData);
+        System.out.println("Total number of deadlines using streams: " + countDeadlinesUsingStream());
+
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -39,11 +43,34 @@ public class Main {
         }
     }
 
+    public static void printDataUsingStream(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using streams");
+        tasksData.stream()
+                .forEach(System.out::println);
+
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static int countDeadlinesUsingStream(ArrayList<Task> tasksData){
+        System.out.println("Calculating count using streams");
+        int count;
+        count = (int) tasksData.stream()
+                .filter((t)->t instanceof Deadline)
+                .count();
+        return count;
+    }
+
+    public static void printDeadlineUsingStream(ArrayList<Task> tasksData){
+        System.out.println("Printing data using streams");
+        tasksData.stream()
+                .filter((t)->t instanceof Deadline)
+                .forEach(System.out::println);
     }
 }
